1ba265a535edd50973ab2b00bdce90640e6a6678
Sintya <81017780+Sintya4@users.noreply.github.com>
2021-05-15 15:44:45 +0700

    DGH BOT

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.Token = "NzkwOTM3MDI0OTQxMTI5NzU5.X-H3gQ.hJkIvLn9W_FUGU7D8BBJ2NQ3vVY"; //Bot Token - Important
+exports.Owner = "767726828311543820"; //Bot Owner ID - Important
+exports.Developer = "740947753135243354"; //Bot Dev ID - Important
+exports.Default_Prefix = "!"; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Support = "https://dsc.gg/mincoder"; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
+exports.Dashboard = "https://bot-jsll.glitch.me/"; //Dashboard Your Bot
+exports.Server_ID = "826826587592065095"; // ID Your Server
+exports.mongodb = "mongodb+srv://Boty:96788312man@cluster0.mtscu.mongodb.net/Maman?retryWrites=true&w=majority";
